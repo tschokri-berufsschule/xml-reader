ea32d63c41ae4ab546d8642fcd54168f498c8d19
lejler <layla.shokri@gmx.de>
2023-05-05 13:57:49 +0200

    added aufgabe 2

--- src/main/java/Auftrag2/ReadBookstoreData2.java
+++ src/main/java/Auftrag2/ReadBookstoreData2.java
@@ -9,20 +9,33 @@ package Auftrag2;
  */
 
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class ReadBookstoreData2 {
 
     public static void main(String[] args) {
-
-
         try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("src/main/resources/buchhandlung2.xml");
+            Node parentNode = doc.getElementsByTagName("buch").item(0);
+            NodeList childNodes = parentNode.getChildNodes();
 
-            // Name der Datei: "src/Auftrag3/buchhandlung.xml"
-            // Add your code here
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                Node child = childNodes.item(i);
 
+                if (child.getNodeType() == Node.ELEMENT_NODE) {
+                    System.out.println(child.getNodeName() + ": " + child.getTextContent());
+                }
+            }
 
         } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
-
 }

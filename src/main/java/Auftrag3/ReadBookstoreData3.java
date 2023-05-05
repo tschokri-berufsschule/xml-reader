28c2e091efd4f186fd1ed9e4b864091ac1514bc5
lejler <layla.shokri@gmx.de>
2023-05-05 14:21:04 +0200

    added aufgabe 3

--- src/main/java/Auftrag3/ReadBookstoreData3.java
+++ src/main/java/Auftrag3/ReadBookstoreData3.java
@@ -9,23 +9,44 @@ package Auftrag3;
  *                    nachname:  Ullenboom 
  */
 
-import javax.xml.parsers.*;
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class ReadBookstoreData3 {
 
 	public static void main(String[] args) {
-
 		try {
-			// Name der Datei: "src/Auftrag3/buchhandlung.xml"
-			// Add your code here
-			
-			
-			
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("src/main/resources/buchhandlung3.xml");
+			Node root = doc.getDocumentElement();
+			printChildNodeValues(root);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
+	}
 
+	public static void printChildNodeValues(Node node) {
+		NodeList childNodes = node.getChildNodes();
+
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node childNode = childNodes.item(i);
+
+			if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+				String nodeName = childNode.getNodeName();
+				String nodeValue = childNode.getTextContent().trim();
+
+				if (!nodeValue.isEmpty() && childNode.getChildNodes().getLength() == 1 && childNode.getFirstChild().getNodeType() == Node.TEXT_NODE) {
+					System.out.println(nodeName + ": " + nodeValue);
+				}
+
+				printChildNodeValues(childNode);
+			}
+		}
 	}
 
 }
